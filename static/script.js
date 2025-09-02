538a94560f3d5b37fbc3879ee27dfc37228e57c9
vichinho <vishoxcl@gmail.com>
2025-09-02 12:24:42 -0400

    cambios index

--- static/script.js
+++ static/script.js
@@ -47,3 +47,28 @@ function createPawCircle() {
 
 setInterval(createPawCircle, 200);
 
+let slideIndex = 0;
+  const slides = document.querySelector('.slides');
+  const totalSlides = slides.children.length;
+
+  function showSlide(index) {
+    if (index >= totalSlides) slideIndex = 0;
+    if (index < 0) slideIndex = totalSlides - 1;
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+  }
+
+  document.querySelector('.next').addEventListener('click', () => {
+    slideIndex++;
+    showSlide(slideIndex);
+  });
+
+  document.querySelector('.prev').addEventListener('click', () => {
+    slideIndex--;
+    showSlide(slideIndex);
+  });
+
+  // Auto-slide cada 5 segundos
+  setInterval(() => {
+    slideIndex++;
+    showSlide(slideIndex);
+  }, 5000);
